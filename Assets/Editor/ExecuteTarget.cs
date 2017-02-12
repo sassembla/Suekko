b79784dfd7ea8d661e4aa50d2074e0006422cdb3
sassembla <toru.inoue@kissaki.tv>
2017-02-13 00:06:42 +0900

    add read.

--- Assets/Editor/ExecuteTarget.cs
+++ Assets/Editor/ExecuteTarget.cs
@@ -4,11 +4,10 @@ public class ExecuteTarget {
 	public string version;
 	public bool overwrite;
 
-
 	public ExecuteTarget () {
 		version = "1.0.0";
 	}
-    
+
 	public void Execute () {
 		Debug.Log("log! overwrite:" + overwrite + " version:" + version);
 	}
---
+++ README.md
@@ -0,0 +1,6 @@
+#Suekko
+
+![loading](./doc/scr.png?raw=true)
+
+make your code to GUI without additional code.
+
