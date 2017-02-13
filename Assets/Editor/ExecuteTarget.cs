9a892bf88cc90a4f71ee4ed23b4bc004124b23b2
sassembla <toru.inoue@kissaki.tv>
2017-02-14 01:16:25 +0900

    updated to enable enum use.

--- Assets/Editor/ExecuteTarget.cs
+++ Assets/Editor/ExecuteTarget.cs
@@ -4,12 +4,21 @@ public class ExecuteTarget {
 	public string version;
 	public bool overwrite;
 
+	public enum MyType {
+		Steak,
+		Ramen,
+		Hamburg
+	}
+	public MyType myType;
+
 	public ExecuteTarget () {
 		version = "1.0.0";
+		overwrite = true;
+		myType = MyType.Hamburg;
 	}
 
 	public void Execute () {
-		Debug.Log("log! overwrite:" + overwrite + " version:" + version);
+		Debug.Log("execute! overwrite:" + overwrite + " version:" + version + " type:" + myType);
 	}
 
 	public void NeverCollectMethod (string s) {}
--- README.md
+++ README.md
@@ -1,8 +1,20 @@
 #Suekko
+ver 0.3.0
+
+create Unity WindowGUI from plane C# code, without additional coding.
 
 ![loading](./doc/scr.png?raw=true)
 
-create Unity WindowGUI from plane code, without additional coding.
+
+
+## surpported types
+public bool, string, enums.
+
+
+## supported methods
+public no parameter methods are available.
 
 ## installation
-wip.
+1. download this repo and copy Assets/SuekkoWindow folder into your Assets folder.
+1. change next line. set your script's type.
+
